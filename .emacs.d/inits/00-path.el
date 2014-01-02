f1797be791aebf80a81bb5b798486604bfaeda98
Ippei Nawate <2648453+sakuna63@users.noreply.github.com>
2014-01-02 13:07:17 +0900

    clean repo

--- .emacs.d/inits/00-path.el
+++
@@ -1,2 +0,0 @@
-;; コマンドパスの設定
-(add-to-list 'exec-path "/usr/local/bin")
--- .emacs.d/inits/20-edit-mode-coffee.el
+++
@@ -1,8 +0,0 @@
-(require 'coffee-mode)
-(defun coffee-mode-hooks ()
-  (and (set (make-local-variable 'tab-width) 2)
-       (set (make-local-variable 'coffee-tab-width) 2))
-  )
-
-(add-hook 'coffee-mode-hook 'coffee-mode-hooks)
-
