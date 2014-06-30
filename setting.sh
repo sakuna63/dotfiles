240df0be09f41a1badba293f7dacd140e62c8559
Ippei Nawate <2648453+sakuna63@users.noreply.github.com>
2014-06-30 16:56:50 +0900

    .gitconfigの追加 - submodule使うのやめるのでantigenをmodulesの外部に移動 - z, zsh-completionsをantigenで管理 - setting.shを作り始めました

---
+++ setting.sh
@@ -0,0 +1,24 @@
+# for create symlink to dotfiles
+gem install homesick
+homesick link
+
+# install homebrew
+if [ `uname` = "Darwin" ]; then
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  brew bundle
+fi
+
+# install neobundle for vim
+mkdir home/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim home/.vim/bundle/
+
+# install antigen for zsh
+git clone https://github.com/zsh-users/antigen home/.zsh.d/
+
+# download git completions
+if [ `uname` = "Linux" ]; then
+  mkdir home/.zsh.d/completion
+  curl -O https://raw.github.com/git/git/master/contrib/completion/git-completion.bash home/.zsh.d/completion/
+  curl -O https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh home/.zsh.d/completion/
+  mv home/.zsh.d/completion/git-completion.zsh home/.zsh.d/completion/_git
+fi
