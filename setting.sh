ff55c6612893f257900a1bb083b9deea48020a8b
Ippei Nawate <2648453+sakuna63@users.noreply.github.com>
2014-07-11 12:48:12 +0900

    setting.shの修正とneobundleの修正

--- setting.sh
+++ setting.sh
@@ -11,20 +11,23 @@ if [ `uname` = "Darwin" ]; then
 fi
 
 # install neobundle for vim
-mkdir home/.vim/bundle
+mkdir -p home/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim home/.vim/bundle/neobundle.vim
+mkdir -p home/.vim/backup
+mkdir -p home/.vim/swap
+mkdir -p home/.vim/undo
 
 # install antigen for zsh
 git clone https://github.com/zsh-users/antigen home/.zsh.d/antigen
 
 # download git completions
 if [ `uname` = "Linux" ]; then
-  mkdir home/.zsh.d/completion
-  curl -O https://raw.github.com/git/git/master/contrib/completion/git-completion.bash home/.zsh.d/completion/
-  curl -O https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh home/.zsh.d/completion/
+  mkdir -p home/.zsh.d/completion
+  curl -O https://raw.github.com/git/git/master/contrib/completion/git-completion.bash home/.zsh.d/completion/git-completion.bash
+  curl -O https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh home/.zsh.d/completion/git-completion.zsh
   mv home/.zsh.d/completion/git-completion.zsh home/.zsh.d/completion/_git
 fi
 
 #download tmux plugins
-git clone https://github.com/erikw/tmux-poweline home/.tmux.d/
+git clone https://github.com/erikw/tmux-poweline home/.tmux.d/tmux-powerline
 
