68a90a04e8fe20c3f62d5cd919f481efa2506cee
Ippei Nawate <2648453+sakuna63@users.noreply.github.com>
2015-06-02 15:59:19 +0900

    Fix

--- bootstrap.d/.zsh
+++ bootstrap.d/.zsh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/zsh-users/antigen ~/.zsh.d/antigen
+if [ -e brew ]; then
+  prefix=$(brew --prefix)
+fi
 
-prefix=$(brew --prefix)
 echo $prefix/bin/zsh | sudo tee -a /etc/shells
 chsh -s $prefix/bin/zsh
