11cf46367815f2575e225a0afa3994ee8f5cc55a
Ippei Nawate <2648453+sakuna63@users.noreply.github.com>
2015-08-09 18:46:11 +0900

    Maintenable adb-run

--- .zsh.d/funcs/adb-run.sh
+++ .zsh.d/funcs/adb-run.sh
@@ -2,104 +2,117 @@
 
 set -e
 
-APK=''
-ACTIVITY=''
-DEVICE_ID=''
-FLAG_INSTALL=0
-
-function show_usage() {
+function show_usage()
+{
   echo "Usage: adb-run -a activity_name -d device_id -f path_to_apk_file -i"
   echo "-i: flag indicating whather attempting to install apk or not"
   exit 1
 }
 
-while getopts a:d:f:hi OPT; do
+while getopts a:d:f:hic OPT
+do
   case $OPT in
-    a ) ACTIVITY=$OPTARG ;;
-    d ) DEVICE_ID=$OPTARG ;;
-    f ) APK=$OPTARG ;;
-    i ) FLAG_INSTALL=1 ;;
+    a ) _ACTIVITY=$OPTARG ;;
+    d ) _DEVICE_ID=$OPTARG ;;
+    f ) _APK=$OPTARG ;;
+    i ) _FLAG_INSTALL=1 ;;
+    c ) _FLAG_COPY_OPTIONS=1 ;;
     h ) show_usage ;;
   esac
 done
 
-### Find target apk file
-if [ -z "$APK" ]; then
-  count_apk=`find . -name '*.apk' | wc -l`
-  if [ $count_apk -eq 0 ]; then
-    echo 'apk not found'
-    exit
-  elif [ $count_apk -eq 1 ]; then
-    APK=`find . -name '*.apk'`
+function find_apk()
+{
+  local apks="$(find . -name '*.apk')"
+  local count=`echo $apks | wc -w`
+  if [ $count -le 1 ]; then
+    echo $apks
   else
-    APK=`find . -name '*.apk' | peco`
+    echo $apks | tr -s ' ' '\n' | peco
   fi
-fi
+}
 
-if [ -z "$APK" -o ! -e "$APK" ]; then
-    echo 'apk not found'
-  exit
-fi
-
-package=`aapt dump badging $APK | head -n 1 | sed -e "s/package:\sname='\(.\+\)'\sversionCode.\+/\1/"`
-
-### Find target activity
-if [ -z "$ACTIVITY" ]; then
-  count_activity=`aapt dump badging $APK | grep launchable-activity | wc -l`
-  if [ $count_activity -eq 0 ]; then
-    echo 'launchable activity not found'
-    exit
-  elif [ $count_activity -eq 1 ]; then
-    ACTIVITY=`aapt dump badging $APK | grep launchable-activity | sed -e "s/.\+name='\(.\+\)'\s\+label.\+/\1/"`
+function find_activity()
+{
+  local activities="`aapt dump badging $1 | grep launchable-activity | sed -e "s/.\+name='\(.\+\)'\s\+label.\+/\1/"`"
+  local count=`echo ${activities} | wc -w`
+  if [ $count -le 1 ]; then
+    echo $activities
   else
-    ACTIVITY=`aapt dump badging $APK | grep launchable-activity | peco | sed -e "s/.\+name='\(.\+\)'\s\+label.\+/\1/"`
+    echo $activities | tr -s ' ' '\n' | peco
   fi
-fi
+}
 
-if [ -z "$ACTIVITY" ]; then
-  echo 'activity not found'
-  exit
-fi
+function find_package()
+{
+  aapt dump badging $1 | head -n 1 | sed -e "s/package:\sname='\(.\+\)'\sversionCode.\+/\1/"
+}
 
-### Find target device
-if [ -z "$DEVICE_ID" ]; then
-  count_device=`adb devices | sed '/^$/d' | wc -l`
-  if [ $count_device -eq 1 ]; then
-    echo "device not found"
-    exit
-  elif [ $count_device -eq 2 ]; then
-    device=`adb devices | sed -e "1,1d"`
+function find_device()
+{
+  count=`adb devices | sed '/^$/d' | wc -l`
+  if [ $count -le 2 ]; then
+    adb devices | sed -e "1,1d" | cut -f 1
   else
-    device=`adb devices | sed -e "1,1d" | peco`
+    adb devices | sed -e "1,1d" | peco | cut -f 1
   fi
+}
 
-  if [ -z "$device" ]; then
-     exit
+function copy_to_clipboard()
+{
+  if which pbcopy >/dev/null 2>&1 ; then
+      # Mac
+      echo $1 | pbcopy
+  elif which xsel >/dev/null 2>&1 ; then
+      # Linux
+      echo $1 | xsel --input --clipboard
+  elif which putclip >/dev/null 2>&1 ; then
+      # Cygwin
+      echo $1 | putclip
+  else
+    echo "copying method does not found."
+    echo "supported command is [pbcopy, xsel, putclip]"
   fi
+}
 
-  DEVICE_ID=`echo $device | cut -d ' ' -f1`
+[ -z "$_APK" ] && _APK=`find_apk`
+if [ ! -e "$_APK" ]; then
+  echo "apk ${_APK} does not found"
+  exit 0
 fi
 
-if [ -z "$DEVICE_ID" ]; then
-  echo 'device not found'
-  exit
+[ -z "$_ACTIVITY" ] && _ACTIVITY=`find_activity ${_APK}`
+if [ -z "$_ACTIVITY" ]; then
+  echo "activity ${_ACTIVITY} does not found"
+  exit 0
 fi
 
+[ -z "$_DEVICE_ID" ] && _DEVICE_ID=`find_device`
+if [ -z "$_DEVICE_ID" ]; then
+  echo "device ${_DEVICE_ID} does not found"
+  exit 0
+fi
+
+package=`find_package ${_APK}`
 ### show command status
 cat << OUTPUT
 --- Command Status ---
-apk      : $APK
+apk      : $_APK
 package  : $package
-activity : $ACTIVITY
-device   : $DEVICE_ID
+activity : $_ACTIVITY
+device   : $_DEVICE_ID
 
 OUTPUT
 
-if [ $FLAG_INSTALL -eq 1 ]; then
-  echo "ADB_SHELL_COMMAND: adb -s $DEVICE_ID install -r -d $APK"
-  adb -s $DEVICE_ID install -r -d $APK
+if [ -n "${_FLAG_INSTALL}" ]; then
+  echo "ADB_SHELL_COMMAND: adb -s $_DEVICE_ID install -r -d $_APK"
+  adb -s $_DEVICE_ID install -r -d $_APK
 fi
 
-echo "ADB_SHELL_COMMAND: adb -s $DEVICE_ID shell am start -n \"$package/$ACTIVITY\" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER"
-adb -s $DEVICE_ID shell am start -n "$package/$ACTIVITY" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+if [ -n "${_FLAG_COPY_OPTIONS}" ]; then
+  echo "option copied"
+  copy_to_clipboard "-a $_ACTIVITY -d $_DEVICE_ID -f $_APK"
+fi
 
+echo "ADB_SHELL_COMMAND: adb -s $_DEVICE_ID shell am start -n \"$package/$_ACTIVITY\" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER"
+adb -s $_DEVICE_ID shell am start -n "$package/$_ACTIVITY" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
